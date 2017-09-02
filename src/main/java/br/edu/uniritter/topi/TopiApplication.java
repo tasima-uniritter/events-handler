8e43a873b2169429e371edb13bca7d8be2a98068
Luis <dev@luiscoms.com.br>
2017-09-01 22:07:08 -0300

    :cry: again

---
+++ src/main/java/br/edu/uniritter/topi/TopiApplication.java
@@ -0,0 +1,12 @@
+package br.edu.uniritter.topi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/edu/uniritter/topi/TopiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.uniritter.topi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
